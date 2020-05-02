18176974e1d1d667af241f83243f7867ee1c0518
佐藤　祐弥 <y.satou@satouyuuwatarunoMacBook-Air.local>
2020-05-02 21:20:59 +0900

    [Finish]chapter1-10

---
+++ work/vagrant/sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '857dd397b0e73f72d5150b8cf04b0c50c030c0492ffb94538df17ea4bbfc3335104ec908d2489605cd72f515ee48c01eb778204d5e0f1040316abd5b7c0d31d4'
